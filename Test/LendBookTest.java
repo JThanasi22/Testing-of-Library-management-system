c0c0ba5b03b55b8b7ed5e3eae891064727ce9914
Suljon Pashaj <128150341+spashaj22@users.noreply.github.com>
2025-01-23 01:14:41 +0100

    Added more tests

--- Test/LendBookTest.java
+++ Test/LendBookTest.java
@@ -1,11 +1,14 @@
+import DB.Books;
 import UI.LendBook;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class LendBookTest {
     private LendBook lendBook;
@@ -20,8 +23,11 @@ public class LendBookTest {
     }
 
     // Tests for setBookId
+
+    // Boundary Value Testing
+
     @Test
-    public void testSetBookId_ValidBookId() {
+    public void testSetBookId_MinValidBookId() {
         int validBookId = 1;
         lendBook.setBookId(validBookId);
         String expectedMessage = "Search and select a member to lend 'Test Book' book";
@@ -29,17 +35,77 @@ public class LendBookTest {
     }
 
     @Test
-    public void testSetBookId_EmptyBookName() {
-        int emptyNameBookId = 2;
-        lendBook.setBookId(emptyNameBookId);
-        assertEquals("Search and select a member to lend '' book", lendBook.infoLabel.getText());
+    public void testSetBookId_JustUnderMin() {
+        int invalidBookId = 0;
+        lendBook.setBookId(invalidBookId);
+        String expectedMessage = "";
+        assertEquals(expectedMessage, lendBook.infoLabel.getText());
+    }
+
+    @Test
+    public void testSetBookId_MaxValidBookId() {
+        int maxValidBookId = Integer.MAX_VALUE;
+        lendBook.setBookId(maxValidBookId);
+        String expectedMessage = "Search and select a member to lend 'Test Book' book";
+        assertEquals(expectedMessage, lendBook.infoLabel.getText());
+    }
+
+    @Test
+    public void testSetBookId_OverMax() {
+        long overMaxBookId = (long) Integer.MAX_VALUE + 1;
+        try {
+            lendBook.setBookId((int) overMaxBookId);
+        } catch (Exception e) {
+            assertTrue(e instanceof IllegalArgumentException || e instanceof SQLException);
+        }
+        String expectedMessage = "";
+        assertEquals(expectedMessage, lendBook.infoLabel.getText());
+    }
+
+    // Class Evaluation testing
+
+    @Test
+    public void testSetBookId_BookExists() {
+        int validBookId = 1;
+        lendBook.setBookId(validBookId);
+        String expectedMessage = "Search and select a member to lend 'Test Book' book";
+        assertEquals(expectedMessage, lendBook.infoLabel.getText());
+    }
+
+    @Test
+    public void testSetBookId_BookDoesNotExist() {
+        int nonExistingBookId = 9999;
+        lendBook.setBookId(nonExistingBookId);
+        String expectedMessage = "";
+        assertEquals(expectedMessage, lendBook.infoLabel.getText());
     }
 
     @Test
     public void testSetBookId_InvalidBookId() {
-        int invalidBookId = 9999;
+        int invalidBookId = -1;
         lendBook.setBookId(invalidBookId);
-        assertEquals("", lendBook.infoLabel.getText());
+        String expectedMessage = "";
+        assertEquals(expectedMessage, lendBook.infoLabel.getText());
+    }
+
+    // Coverage Testing
+
+    @Test
+    public void testSetBookId_SQLExceptionThrown() {
+        int invalidBookId = -999;
+
+        lendBook.setBookId(invalidBookId);
+        String expectedMessage = "";
+        assertEquals(expectedMessage, lendBook.infoLabel.getText());
+    }
+
+    // Other Tests
+
+    @Test
+    public void testSetBookId_EmptyBookName() {
+        int emptyNameBookId = 2;
+        lendBook.setBookId(emptyNameBookId);
+        assertEquals("Search and select a member to lend '' book", lendBook.infoLabel.getText());
     }
 
     // Tests for lendButtonActionPerformed
